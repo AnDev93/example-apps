4e8a1df0ec4ff3c8bbf45be728add7e4bf444053
AnDev <andersosanchez_911@hotmail.com>
2026-06-18 08:19:28 -0400

    Agregar archivos de configuración de aplicación y servidor, y crear archivo .htaccess

---
+++ config/SERVER.php
@@ -0,0 +1,16 @@
+<?php
+	
+	/*----------  Datos del servidor  ----------*/
+	const SERVER="localhost";
+	const DB=""; // nombre de base datos
+	const USER="root"; // nombre del usuario mysql
+	const PASS=""; //contrase#a del usuario mysql
+
+
+	const SGBD="mysql:host=".SERVER.";dbname=".DB;
+
+
+	/*----------  Datos de la encriptacion (No modificar) ----------*/
+	const METHOD="AES-256-CBC"; //Cifrado de 256 bits con CBC// no modificar//
+	const SECRET_KEY='APP2026'; // solo modificar // 
+	const SECRET_IV='102791'; // 16 caracteres para AES-256-CBC// no modificar//
